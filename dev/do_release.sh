5cefa6f9446d45f104bb5e821763132abe057b39
Akash Rawal <argmlac@gmail.com>
2024-10-08 20:16:09 +0530

    Add release steps for rust

---
+++ dev/do_release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+version="$1"
+if test -z "$version"; then
+	echo "Usage: $0 <version>" >&2
+	exit 1
+fi
+
+#Check version in each cargo.toml
+for file in protocol/Cargo.toml rust/Cargo.toml; do
+	file_version="`sed -nr -e 's/.*version[ =]*"([^"]*)".*/\1/p' "$file"`"
+
+	if test "$file_version" != "$version"; then
+		echo "$file: Version mismatch: change $file_version -> $version" >&2
+		exit 1
+	fi
+done
+
+git tag -m "Release $version" "r${version}"
+

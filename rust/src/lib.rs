fe60946f7d953dfe469755840fc2099e20d2187c
Akash Rawal <argmlac@gmail.com>
2024-10-08 22:00:26 +0530

    Protocol re-exports should be their own module.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,6 +3,9 @@ pub mod args;
 pub mod context;
 pub mod container;
 
-pub use disposables_protocol::*;
+//Re-exports
+pub mod protocol {
+    pub use disposables_protocol::*;
+}
 pub use context::Context;
 pub use container::{Container, ContainerParams};
